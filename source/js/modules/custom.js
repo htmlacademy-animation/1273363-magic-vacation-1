f88a997f5b789a1ed82978baf377ec921abe4a49
Michael <moskvichevmihail60@yandex.ru>
2022-09-28 19:06:25 +0300

    Добавлена реализация третьего задания первой главы

--- source/js/modules/custom.js
+++ source/js/modules/custom.js
@@ -1,7 +1,6 @@
 export default () => {
-  window.onload = function () {
+  document.addEventListener(`DOMContentLoaded`, function () {
     let bodyDOM = document.querySelector(`body`);
-
     bodyDOM.classList.add(`show-anim`);
-  };
+  });
 };
