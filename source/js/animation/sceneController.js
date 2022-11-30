58bb4c21be748242fff4c478947ac10f43dcd263
Mikhail Moskvichev <moskvichevmihail60@yandex.ru>
2022-11-30 18:30:13 +0300

    Добавлена реализация девятого задания шестой главы

--- source/js/animation/sceneController.js
+++ source/js/animation/sceneController.js
@@ -1,16 +1,15 @@
 import {scene} from "./initAnimationScreen";
-import {SceneWithLantern} from '../scenes/SceneWithLantern';
 import {LatheGeometryCreator} from "./LatheGeometryCreator";
-import {Saturn} from "./objects/Saturn";
 import {MaterialCreator} from "./MaterialCreator";
-import * as THREE from "three";
-import {MainPageComposition} from "./objects/MainPageComposition";
-import {Road} from "./objects/Road";
-import {Carpet} from "./objects/Carpet";
+import {MainPageScene} from "../scenes/main-page/MainPageScene";
 import {SvgPathLoader} from "../helpers/SvgPathLoader";
-import {EXTRUDE_SETTINGS, MATERIAL_TYPE, SVG_ELEMENTS} from "../constants";
+import {EXTRUDE_SETTINGS, SVG_ELEMENTS} from "../constants";
 import {ExtrudeSvgCreator} from "../helpers/ExtrudeSvgCreator";
 import {ObjectsCreator} from "../helpers/ObjectCreator";
+import {RoomsPageScene} from "../scenes/room-page/RoomsPageScene";
+import {degreesToRadians} from "../utils/degreesToRadians";
+import {TransformationGuiHelper} from "../helpers/TransformationGuiHelper";
+import {PageSceneCreator} from "../scenes/PageSceneCreator";
 
 const materialCreator = new MaterialCreator();
 const latheGeometryCreator = new LatheGeometryCreator();
@@ -19,117 +18,51 @@ const extrudeSvgCreator = new ExtrudeSvgCreator(
   svgShapeLoader,
   EXTRUDE_SETTINGS
 );
-const objectCreator = new ObjectsCreator(materialCreator);
+const objectCreator = new ObjectsCreator();
+const transformationGuiHelper = new TransformationGuiHelper();
+const pageSceneCreator = new PageSceneCreator(
+  materialCreator,
+  extrudeSvgCreator,
+  objectCreator,
+  latheGeometryCreator,
+  transformationGuiHelper
+);
 
 export const sceneController = {
   clearScene() {
     scene.clearScene();
   },
 
-  addRoadAndCarpet() {
-    const road = new Road(latheGeometryCreator, materialCreator);
-    const carpet = new Carpet(latheGeometryCreator, materialCreator);
-    road.position.set(0, 100, 0);
-
-    scene.addSceneObject(road);
-    scene.addSceneObject(carpet);
-  },
-
-  addSaturn() {
-    const saturn = new Saturn(materialCreator, { darkMode: false });
-
-    saturn.position.set(0, 500, 0);
-
-    scene.addSceneObject(saturn);
-  },
-
-  addDarkSaturn() {
-    const saturn = new Saturn(materialCreator, { darkMode: true });
-
-    saturn.position.set(300, 500, 0);
-
-    scene.addSceneObject(saturn);
-  },
-
-  addSpheres() {
-    const spheresGroup = new THREE.Group();
-
-    const geometry = new THREE.SphereGeometry(100, 32, 32);
-
-    const sphere1 = new THREE.Mesh(geometry);
-    const sphere2 = new THREE.Mesh(geometry);
-    const sphere3 = new THREE.Mesh(geometry);
-    const sphere4 = new THREE.Mesh(geometry);
-    const sphere5 = new THREE.Mesh(geometry);
-    const sphere6 = new THREE.Mesh(geometry);
-
-    sphere1.position.set(-110, 110, 0);
-    sphere2.position.set(-110, -110, 0);
-    sphere3.position.set(110, 110, 0);
-    sphere4.position.set(110, -110, 0);
-    sphere5.position.set(330, 110, 0);
-    sphere6.position.set(330, -110, 0);
-
-    sphere1.material = materialCreator.create(MATERIAL_TYPE.SoftMaterial, {
-      color: MaterialCreator.Colors.Blue,
-    });
-
-    sphere2.material = materialCreator.create(MATERIAL_TYPE.SoftMaterial, {
-      color: MaterialCreator.Colors.DarkBlue,
-    });
-
-    sphere3.material = materialCreator.create(MATERIAL_TYPE.BasicMaterial, {
-      color: MaterialCreator.Colors.Blue,
-    });
-
-    sphere4.material = materialCreator.create(MATERIAL_TYPE.BasicMaterial, {
-      color: MaterialCreator.Colors.DarkBlue,
-    });
-
-    sphere5.material = materialCreator.create(MATERIAL_TYPE.StrongMaterial, {
-      color: MaterialCreator.Colors.Blue,
-    });
-
-    sphere6.material = materialCreator.create(MATERIAL_TYPE.StrongMaterial, {
-      color: MaterialCreator.Colors.DarkBlue,
-    });
-
-    spheresGroup.add(sphere1);
-    spheresGroup.add(sphere2);
-    spheresGroup.add(sphere3);
-    spheresGroup.add(sphere4);
-    spheresGroup.add(sphere5);
-    spheresGroup.add(sphere6);
-
-    spheresGroup.translateY(-400);
-
-    scene.addSceneObject(spheresGroup);
-  },
-
   addMainPageComposition() {
-    const mainPageComposition = new MainPageComposition(
-      materialCreator,
-      extrudeSvgCreator,
-      objectCreator
-    );
-
+    const mainPageComposition = new MainPageScene(pageSceneCreator);
     scene.addSceneObject(mainPageComposition);
   },
 
-  addSceneWithLantern() {
-    scene.addSceneObject(new SceneWithLantern(materialCreator));
+  addRoomsPageComposition() {
+    const positionZ = 2550;
+    const positionY = 800;
+
+    scene.camera.position.set(0, positionY, positionZ);
+
+    scene.controls.target.set(
+      0,
+      positionY - positionZ * Math.tan(degreesToRadians(15)),
+      0
+    );
+
+    const roomsComposition = new RoomsPageScene(pageSceneCreator);
+
+    roomsComposition.rotateY(-Math.PI / 4);
+    roomsComposition.rotateY(-Math.PI / 2);
+    roomsComposition.rotateY(-Math.PI / 2);
+
+    scene.addSceneObject(roomsComposition);
+    scene.addTransformationsToLoop([()=>{
+      roomsComposition.rotateY(-0.003)
+    }])
   },
 
   addScreenMesh() {
-    // this.addSceneWithLantern();
-
-    // this.addSpheres();
-
-    // this.addSaturn();
-    // this.addDarkSaturn();
-
-    this.addMainPageComposition();
-
-    //this.addRoadAndCarpet();
+    this.addRoomsPageComposition();
   },
 };
