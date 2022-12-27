145faa81469547245942165f23235f7b0fd60232
Michael <moskvichevmihail60@yandex.ru>
2022-12-27 19:20:45 +0300

    Правки перед сдачей проекта

--- source/js/modules/full-page-scroll.js
+++ source/js/modules/full-page-scroll.js
@@ -200,7 +200,6 @@ export default class FullPageScroll {
    */
   transitionBetweenScreenWithOpacity(prevActiveScreen, nextActiveScreen) {
     prevActiveScreen.classList.add("screen--fadeOut");
-    this.transitionBackground.classList.add(`transition--background__show`);
 
     setTimeout(() => {
       prevActiveScreen.classList.remove(`screen--fadeOut`);
@@ -210,9 +209,6 @@ export default class FullPageScroll {
 
       prevActiveScreen.classList.remove(`active`);
       prevActiveScreen.classList.add(`screen--hidden`);
-      this.transitionBackground.classList.remove(
-        `transition--background__show`
-      );
     }, 400);
   }
 
